99caac1c2486f9954ac94061fa4d1e036722506a
Diurno <Diurno@DAM111>
2026-04-15 13:26:40 +0200

    holi

---
+++ Pogramacion/src/ud10/EjerciciosLista1/Ejercicio5.java
@@ -0,0 +1,14 @@
+package ud10.EjerciciosLista1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejercicio5 {
+	public static void main(String[] args) {
+		
+		List<Integer> lista = new ArrayList<>();
+		
+		lista.add("Alumno1");
+		
+	}
+}
